22e7681cd45dbe220f71efd155e5c9d85f847759
Maidul Bappy <maidul.bappy.001@gmail.com>
2021-03-25 19:21:38 +0600

    url correction

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "daa8b8541b6bcf70f746cd7637c98c4f";
+const apiKey = 'b6cc4392568a3586e950307c86a22bbd';
 const apiBase = 'https://api.openweathermap.org/data/2.5/weather';
 
 const getWeatherData = city => {
